395f48b7d77d4cf7e7e2dc17847e938f0355bcea
Aaron Watters <awatters@simonsfoundation.org>
2015-10-13 15:36:43 -0400

    basic function and method calls seem to work

--- jp_gene_viz/jp_cytoscape.js
+++ jp_gene_viz/jp_cytoscape.js
@@ -1,11 +1,12 @@
 
-require(["widgets/js/widget", "widgets/js/manager", "cytoscape"], function(widget, manager, cytoscape) {
+require(["widgets/js/widget", "widgets/js/manager", "cytoscape", "underscore", "jquery"
+], function(widget, manager, cytoscape, _, $) {
 	debugger;
 
 	var CytoScapeView = widget.DOMWidgetView.extend({
 
 		render: function() {
-			debugger;
+			//debugger;
 			var that = this;
 			var $el = that.$el;
             var target = document.createElement("div");
@@ -13,19 +14,111 @@ require(["widgets/js/widget", "widgets/js/manager", "cytoscape"], function(widge
             target.style.height = "100px";
             var $target = $(target);
             $el.append(target);
+            that.$target = $target;
             //$el.text("Hello world!");
             //return;
-			var cy = $target.cytoscape({
+            var style = {
+                "color": "red",
+                "background-color": "blue"
+            }
+			var cy = cytoscape({
+                container: target,
                 elements: {
-                    nodes:[{ data: { id: "n", label: "cytoscape.js"}} ]
+                    nodes:[{ data: { id: "n", label: "cytoscape.js", style: style}} ]
                 },
                 style: 'node { content: data(label); }',
 				ready: function() { 
-                    debugger;
+                    //debugger;
                     console.log("cytoscape is ready"); 
                 }
 			});
-		}
+            that.cy = cy;
+            that.update();
+            // for debugging only
+            window.cy = cy;
+		},
+
+        update: function(options) {
+            var that = this;
+            var commands = this.model.get("commands");
+            var width = this.model.get("width");
+            var height = this.model.get("height");
+            var $target = that.$target;
+            var target = $target[0];
+            var cy = that.cy;
+            // set width and height of container
+            $target.width(width);
+            $target.height(height);
+            // execute any commands, in batch mode.
+            if (commands.length == 2) {
+                var command_counter = commands[0];
+                var command_list = commands[1];
+                var some_command = false;
+                var results = [];
+                cy.batch(function() {
+                    _.each(command_list, function(command, i) {
+                        some_command = true;
+                        var result = that.execute_command(command);
+                        results[i] = that.json_safe(result);
+                    });
+                });
+            }
+            // reset the commands list, if appropriate.
+            if (some_command) {
+                that.model.set("commands", []);
+                that.model.set("results", [command_counter, results])
+                that.touch();
+            }
+        },
+
+        execute_command: function(command) {
+            // see jp_cytoscope.py send_commands docstring for explanation.
+            var that = this;
+            var result = command;   // default
+            if ($.isArray(command)) {
+                var cy = that.cy;
+                var indicator = command[0];
+                var remainder = command.slice();
+                remainder.shift();
+                if (indicator == "fun") {
+                    var name = remainder.shift();
+                    var args = remainder.map(that.execute_command);
+                    var fn = cy[name];
+                    result = fn.apply(cy, args);
+                } else if (indicator == "method") {
+                    var target_desc = remainder.shift();
+                    var target = that.execute_command(target_desc);
+                    var name = remainder.shift();
+                    var args = remainder.map(that.execute_command);
+                    var method = target[name];
+                    result = method.apply(target, args);
+                } else if (indicator == "list") {
+                    result = remainder.map(that.execute_command);
+                } else if (indicator == "dict") {
+                    result = {};
+                    var desc = remainder[0];
+                    for (var key in desc) {
+                        var key_desc = desc[key];
+                        var val = that.execute_command(key_desc);
+                        result[key] = val;
+                    }
+                } else if (indicator == "id") {
+                    // untranslated object
+                    return remainder[0]
+                }
+            }
+            return result;
+        },
+
+        json_safe: function(val) {
+            // maybe expand later as need arises
+            var ty = (typeof val);
+            if ((ty == "number") || (ty == "string") || (ty == "boolean")) {
+                return val;
+            }
+            return null;
+        }
+
 	});
 
 	manager.WidgetManager.register_widget_view('CytoScapeView', CytoScapeView);
--- jp_gene_viz/jp_cytoscape.py
+++ jp_gene_viz/jp_cytoscape.py
@@ -6,6 +6,7 @@ from traitlets import Unicode, Float, List, Dict
 import json
 import os
 import pprint
+import types
 
 def load_javascript_support(verbose=False):
     """
@@ -19,10 +20,106 @@ def load_javascript_support(verbose=False):
             print("loading javascript from", repr(js_filename))
         display(Javascript(js_filename))
 
+
+def validate_commands(commands, top=True):
+    """
+    Validate command sequence ala send_commands.
+    """
+    return [validate_command(x, top) for x in commands]
+
+
+def validate_command(command, top=True):
+    """
+    Simple sanity checks on commands to the javascript view
+    as described in the send_commands docstring.
+    """
+    if type(command) is types.ListType:
+        indicator = command[0]
+        remainder = command[1:]
+        if indicator == "fun":
+            name = remainder[0]
+            args = remainder[1:]
+            assert type(name) is types.StringType, (
+                "function name must be string " + repr(name))
+            args = validate_commands(args, top=False)
+            remainder = [name] + args
+        elif indicator == "method":
+            target = remainder[0]
+            name = remainder[1]
+            args = remainder[2:]
+            assert type(name) is types.StringType, (
+                "method name must be string " + repr(name))
+            validate_command(target)
+            validate_commands(args, top=False)
+            remainder = [target, name] + args
+        elif indicator == "list":
+            remainder = validate_commands(remainder, top=False)
+        elif indicator == "dict":
+            [d] = remainder
+            d = dict((k, validate_command(d[k], top=False)) for k in d)
+            remainder = [d]
+        elif indicator == "id":
+            # Any json value is okay
+            assert len(remainder) == 1, (
+                "id takes one object only. " + repr(remainder))
+        else:
+            raise ValueError("bad indicator " + repr(indicator))
+        command = [indicator] + remainder
+    elif top:
+        assert type(command) is types.ListType, (
+            "top level command must be list " + repr(command))
+    return command
+
 class CytoscapeWidget(widgets.DOMWidget):
     """
     Jupyter notebook widget which presents a Cytoscope.js visualization.
     """
 
     _view_name = Unicode("CytoScapeView", sync=True)
+
+    # The list of commands for view to execute (see send_commands)
+    # form: [counter, commands]
+    # counter is used for bookkeeping.
+    commands = List([], sync=True)
+
+    # List of results from command execution
+    results = List([], sync=True)
+
+    event_data = Dict({}, sync=True)
+
+    width = Unicode("100px", sync=True)
+
+    height = Unicode("100px", sync=True)
+
+    def __init__(self, *pargs, **kwargs):
+        super(CytoscapeWidget, self).__init__(*pargs, **kwargs)
+        self.commands_count = 0
+        self.count_to_results_callback = {}
+
+    def send_commands(self, commands_iter, results_callback=None):
+        """
+        Send a batch sequence of commands as json lists to the javascript view.
+        The commands_iter are executed in the cytoscape context $cy using a recursive
+        functional mini interpreter I which translates JSON objects to javascript
+        actions.
+
+        I(["fun", name, arg0, arg1...]) --> $cy.name(I(arg0), I(arg1)...)
+        I(["method", target, name, arg0...]) --> I(target).name(I(arg0)...)
+        I(["id", x]) --> x (untranslated json object)
+        I(["list", x0, x1, ..]) --> [I(x0), I(x1), ...] (translated sequence)
+        I(["dict", {k0: x0, ...}]) --> {k0: I(x0), ...} (translated dict)
+        I(not_a_list) --> not_a_list (untranslated value)
+
+        For example to invoke cy.$("#j").addClass("funny") use command
+        ["method", ["fun", "$", "#j"], "addClass", "funny"]
+
+        The results of the command sequence are passed from the javascript view
+        except that results which are not json compatible are mapped to None.
+        If provided results_callback(results) is called for results of a command.
+        """
+        count = self.commands_count
+        self.commands_count = count + 1
+        commands = validate_commands(list(commands_iter))
+        # send commands to view by modifying the commands trait.
+        self.commands = [count, commands]
 
