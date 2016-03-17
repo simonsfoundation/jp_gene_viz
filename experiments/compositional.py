e6101dda4e554e3829f3635fe734df55c24c646e
Aaron Watters <awatters@simonsfoundation.org>
2016-03-17 13:16:39 -0400

    Inverse CLR experiment

--- experiments/compositional.py
+++ experiments/compositional.py
@@ -34,6 +34,9 @@ def clr(vector):
     product_root = vector.prod(0) ** (1.0/d)
     return np.log(vector / product_root)
 
+def invclr(vector):
+    return closure(np.exp(vector))
+
 def clrs(vectors):
     "clr where vectors with zero components are dropped."
     vectors = strictly_positive(vectors)
@@ -44,6 +47,9 @@ def clrs(vectors):
     nz_vectors = vectors[not_zeros]
     nz = len(nz_product_roots)
     return np.log(nz_vectors / nz_product_roots.reshape(nz, 1))
+
+def invclrs(vectors):
+    return closures(np.exp(vectors))
 
 def xpoints3d(xmin, xmax, num, y, z):
     xs = np.linspace(xmin, xmax, num)
@@ -131,6 +137,23 @@ def globe(offset, radius, n=50, m=8):
         result.append(a)
     return result
 
+# A subspace basis for hyperplane x+y+z+w = 0
+basis = np.array([
+        (1, 0, 0, -1),
+        (0, 1, -1, 0),
+        (0.5, -0.5, -0.5, 0.5)
+    ])
+
+inv_basis = np.linalg.inv(basis[:, :-1])
+
+def to4d(vectors3d):
+    "project 3d vectors onto hyperplane x+y+z+w=0"
+    return vectors3d.dot(basis)
+
+def to3d(hyperplane_vectors4d):
+    "reverse project hyperplane vectors to 3d"
+    return hyperplane_vectors4d[:, :-1].dot(inv_basis)
+
 class Diagram(traitlets.HasTraits):
 
     cleanup = None
@@ -139,16 +162,20 @@ class Diagram(traitlets.HasTraits):
     xy_rotation = traitlets.Float(0)
     xz_rotation = traitlets.Float(0)
     yz_rotation = traitlets.Float(0)
+    offset = traitlets.Float(4.0)
+    max_angle = traitlets.Float(0.4)
+    delta_angle = traitlets.Float(0.02)
 
     def __init__(self, *args, **kwargs):
         super(Diagram, self).__init__(*args, **kwargs)
+        mx = self.max_angle * 2
         d = self.diagram = js_proxy.ProxyWidget()
         xy = self.xy_slider = widgets.FloatSlider(
-            description="XY rotation", min=-1, max=1)
+            description="XY rotation", min=-mx, max=mx)
         xz = self.xy_slider = widgets.FloatSlider(
-            description="XZ rotation", min=-1, max=1)
+            description="XZ rotation", min=-mx, max=mx)
         yz = self.xy_slider = widgets.FloatSlider(
-            description="YZ rotation", min=-1, max=1)
+            description="YZ rotation", min=-mx, max=mx)
         traitlets.link((self, "xy_rotation"), (xy, "value"))
         traitlets.link((self, "xz_rotation"), (xz, "value"))
         traitlets.link((self, "yz_rotation"), (yz, "value"))
@@ -165,13 +192,22 @@ class Diagram(traitlets.HasTraits):
         import time
         theta = 0
         while True:
-            theta += 0.02
+            theta += self.delta_angle
+            mx = self.max_angle
             self.do_draw = False
-            self.xz_rotation = np.cos(theta)*0.4
-            self.xy_rotation = np.sin(theta*1.1)*0.4
+            self.xz_rotation = np.cos(theta)*mx
+            self.xy_rotation = np.sin(theta*1.1)*mx
             self.do_draw = True
-            self.yz_rotation = np.sin(theta*0.4)*0.4
+            self.yz_rotation = np.sin(theta*0.4)*mx
             time.sleep(0.1)
+
+    def draw_simplex(self, d, scene, THREE):
+        last = [0, 0, 1]
+        origin = [0, 0, 0]
+        for next in [[1, 0, 0], [0, 1, 0], [0, 0, 1]]:
+            d(THREE.simple_curve(scene, [origin, next], 0xdddddd))
+            d(THREE.simple_curve(scene, [last, next], 0xff0000))
+            last = next
 
     def draw_diagram(self):
         if not self.do_draw:
@@ -185,21 +221,28 @@ class Diagram(traitlets.HasTraits):
         new = d.save_new
         scene = new("scene", THREE.Scene, [])
         camera = new("camera", THREE.PerspectiveCamera, [55, 1.0, 1, 100000])
-        offset = 4.0
+        offset = self.offset
         d(camera.position._set("x", offset*2/3.)._set("y", offset)._set("z", offset*3/2.))
         d(camera.lookAt(scene.position))
         renderer = new("renderer", THREE.WebGLRenderer, [])
         d(renderer.setSize(800, 800))
         d(element.append(renderer.domElement))
         # Draw the diagram
-        last = [0, 0, 1]
-        origin = [0, 0, 0]
-        for next in [[1, 0, 0], [0, 1, 0], [0, 0, 1]]:
-            d(THREE.simple_curve(scene, [origin, next], 0xdddddd))
-            d(THREE.simple_curve(scene, [last, next], 0xff0000))
-            last = next
+        self.draw_simplex(d, scene, THREE)
         #lines = xygridlines(0.1, 1.5, 10, 20, 1.5)
         #lines = great_circles(1.2, 1.1)
+        self.draw_curves(d, scene, THREE)
+        d(renderer.render(scene, camera))
+        d.flush()
+        def cleanup():
+            "three.js cleanup -- finalize the scene, etcetera"
+            d(THREE.simple_curve.dispose_all(scene))
+            d(element.empty())
+            #d.flush()
+        # defint a cleanup action
+        self.cleanup = cleanup
+
+    def draw_curves(self, d, scene, THREE):
         lines = globe(2, 1)
         for line in lines:
             line = xzrotations(line, self.xz_rotation)
@@ -214,12 +257,24 @@ class Diagram(traitlets.HasTraits):
             ccline = clrs(line)
             curve = ccline.tolist()
             d(THREE.simple_curve(scene, curve, 0x55aaaa))
-        d(renderer.render(scene, camera))
-        d.flush()
-        def cleanup():
-            "three.js cleanup -- finalize the scene, etcetera"
-            d(THREE.simple_curve.dispose_all(scene))
-            d(element.empty())
-            #d.flush()
-        # defint a cleanup action
-        self.cleanup = cleanup
+
+class InvDiagram(Diagram):
+    
+    offset = traitlets.Float(2.0)
+    max_angle = traitlets.Float(6.0)
+
+    def draw_curves(self, d, scene, THREE):
+        lines = globe(1, 0.5)
+        for line in lines:
+            line = xzrotations(line, self.xz_rotation)
+            line = xyrotations(line, self.xy_rotation)
+            line = yzrotations(line, self.yz_rotation)
+            curve = line.tolist()
+            d(THREE.simple_curve(scene, curve, 0x55aaaa))
+            line4 = to4d(line)
+            lineinv = invclrs(line4)
+            #line3 = to3d(lineinv)
+            line3 = lineinv[:, :-1]  # just throw out the last dimension...
+            curve = line3.tolist()
+            d(THREE.simple_curve(scene, curve, 0x55ff55))
+
