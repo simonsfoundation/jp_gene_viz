375158d1a1b8ae5d751bef887295cbfc54dfcea4
Aaron Watters <awatters@simonsfoundation.org>
2017-01-31 13:22:48 -0500

    parameterized surface and curve in doodle3d

--- jp_gene_viz/doodle3d.py
+++ jp_gene_viz/doodle3d.py
@@ -4,6 +4,58 @@ Quick helper for creating 3d diagrams with orbit controls in Jupyter
 
 from jp_gene_viz import js_context
 from jp_gene_viz import js_proxy
+import numpy as np
+from numpy.linalg import norm
+
+
+def parameterized_points_1d(f, min_t, max_t, npoints):
+    t_values = np.linspace(min_t, max_t, npoints)
+    return np.array([f(t) for t in t_values], dtype=np.float)
+
+def parameterized_points_2d(f, mins, maxes, counts):
+    "generate 2d array of triples using f(u,v) --> (x, y, z)"
+    (umin, vmin) = mins
+    (umax, vmax) = maxes
+    (ucount, vcount) = counts
+    u_values = np.linspace(umin, umax, ucount)
+    v_values = np.linspace(vmin, vmax, vcount)
+    ur = range(ucount)
+    vr = range(vcount)
+    result = np.zeros((ucount, vcount, 3))
+    for i in ur:
+        u = u_values[i]
+        for j in vr:
+            v = v_values[j]
+            result[i, j] = f(u, v)
+    return result
+
+def triangulate_2d_points(array):
+    "generate list of points and triangulations from array of triples."
+    (u_count, v_count, dimension) = array.shape
+    assert dimension == 3
+    npoints = u_count * v_count
+    points = [None] * npoints
+    def point_ravel(i, j):
+        return i + (u_count * j)
+    for i in range(u_count):
+        for j in range(v_count):
+            ij = point_ravel(i, j)
+            assert points[ij] is None
+            points[ij] = array[i][j]
+    assert None not in points
+    ntriangles = 2 * (u_count - 1) * (v_count - 1)
+    triangles = set()
+    for i in range(u_count - 1):
+        for j in range(v_count - 1):
+            a = point_ravel(i, j)
+            b = point_ravel(i, j+1)
+            c = point_ravel(i+1, j+1)
+            d = point_ravel(i+1, j)
+            assert max(a, b, c, d) < npoints
+            triangles.add((a, b, d))
+            triangles.add((d, b, c))
+    assert len(triangles) == ntriangles
+    return (points, list(triangles))
 
 def init():
     js_context.load_if_not_loaded(["three.js"], local=False)
@@ -55,6 +107,15 @@ class Doodle3D(object):
         THREE = self.THREE
         w(THREE.simple_curve(self.scene, points, color))
 
+    def parameterized_curve(self, color, f, min_t, max_t, npoints):
+        points = parameterized_points_1d(f, min_t, max_t, npoints)
+        return self.curve(color, points)
+
+    def parameterized_surface(self, f, mins, maxes, counts, color, kind="solid", opacity=None):
+        A = parameterized_points_2d(f, mins, maxes, counts)
+        (points, indices) = triangulate_2d_points(A)
+        return self.triangle_surface(points, indices, color, kind, opacity)
+
     def triangle_surface(self, points, indices, color, kind="solid", opacity=None):
         assert kind in ["wire", "solid"]
         points = list(map(list, points))
@@ -76,6 +137,38 @@ class Doodle3D(object):
         w = self.w
         new_light = w.save_new("light", self.THREE.AmbientLight, [color])
         w(self.scene.add(new_light))
+
+    def pointer(self, from_point, to_point, color, 
+            kind="solid", base_ratio=0.1, epsilon=1e-3, strict=True):
+        from_point = np.array(from_point)
+        to_point = np.array(to_point)
+        diff = to_point - from_point
+        ndiff = norm(diff)
+        if ndiff < epsilon:
+            if strict:
+                raise ValueError("point difference is too small for pointer")
+            else:
+                return #  silently ignore
+        direction = diff / ndiff
+        orthogonal1 = None
+        for axis_point in [(1,0,0), (0,1,0), (0,0,1)]:
+            orthogonal1 = np.cross(direction, axis_point)
+            norm1 = norm(orthogonal1)
+            if norm1 > epsilon:
+                break
+        assert orthogonal1 is not None
+        orthogonal1 = orthogonal1 / norm1
+        orthogonal2 = np.cross(direction, orthogonal1)
+        #print "orthogonals", direction, orthogonal1, orthogonal2
+        norm2 = norm(orthogonal2)
+        assert norm2 > epsilon
+        orthogonal2 = orthogonal2/norm2
+        p1 = from_point + (ndiff * base_ratio) * orthogonal1
+        p2 = from_point + (ndiff * base_ratio) * orthogonal2
+        p3 = from_point - (ndiff * base_ratio * 0.4) * (orthogonal1 + orthogonal2)
+        points = [to_point, p1, p2, p3]
+        triples = [(1,2,3), (0,3,2), (3,0,1), (1,0,2)]
+        self.triangle_surface(points, triples, color, kind)
 
     def std_lighting(self, offset=1000):
         self.light(0x915f7a, offset, offset, offset)
