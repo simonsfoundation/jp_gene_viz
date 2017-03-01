8db07b97c19c8d72eadb25c02ec632a6ec0a7e54
Aaron Watters <awatters@simonsfoundation.org>
2017-03-01 09:19:38 -0500

    emulate svg event handling (limited)

--- jp_gene_viz/proxy_html5_canvas.py
+++ jp_gene_viz/proxy_html5_canvas.py
@@ -29,6 +29,14 @@ class HTML5CanvasProxy(traitlets.HasTraits):
     # Canvas height
     svg_height = traitlets.Float(SIDE_DEFAULT, sync=True)
 
+    # White separated names of event to watch
+    watch_event = traitlets.Unicode("", sync=True)
+    
+    # White separated names of event to unwatch
+    unwatch_event = traitlets.Unicode("", sync=True)
+
+    default_event_callback = None
+
     def __init__(self, viewBox="0 0 500 500", dimension=SIDE_DEFAULT, *pargs, **kwargs):
         super(HTML5CanvasProxy, self).__init__(*pargs, **kwargs)
         self.viewBox = viewBox
@@ -36,6 +44,8 @@ class HTML5CanvasProxy(traitlets.HasTraits):
         self.svg_height = dimension
         self.on_trait_change(self.change_dimensions, "svg_width")
         self.on_trait_change(self.change_dimensions, "svg_height")
+        self.on_trait_change(self.start_watch_event, "watch_event")
+        self.on_trait_change(self.stop_watch_event, "unwatch_event")
         self.font = "Arial"  # default
         self.font_size = 10
         self.font_style = ""
@@ -45,6 +55,36 @@ class HTML5CanvasProxy(traitlets.HasTraits):
         w = self.widget = js_proxy.ProxyWidget()
         self.element = w.element()
         self.empty()
+
+    def start_watch_event(self):
+        events = self.watch_event
+        w = self.widget
+        e = self.element
+        callback = w.callback(self.handle_event, "dummy value", level=3)
+        w(e.on(events, callback))
+        w.flush()
+
+    def handle_event(self, identifier, args):
+        event = args["0"]
+        offsetX = event["offsetX"]
+        offsetY = event["offsetY"]
+        (x, y) = self.offset_to_point(offsetX, offsetY)
+        # emulate svg
+        event["svgX"] = x
+        event["svgY"] = y
+        import pprint
+        #print "event at", (x,y), "from offsets", (offsetX, offsetY)
+        #pprint.pprint(event)
+        cb = self.default_event_callback
+        if cb is not None:
+            cb(event)
+
+    def stop_watch_event(self):
+        events = self.unwatch_event
+        w = self.widget
+        e = self.element
+        w(e.off(events))
+        w.flush()
 
     def set_view_box(self, x, y, w, h):
         #p "viewbox", (x,y,w,h)
@@ -84,18 +124,31 @@ class HTML5CanvasProxy(traitlets.HasTraits):
     def assignment_cmd(self, lhs, rhs):
         return [ASSIGN, lhs, rhs]
 
+    send_count = 0
+
     def send_commands(self):
+        self.send_count += 1
         w = self.widget
         elt = self.element
         command_prefix = []
         if self.is_empty:
             # Initialize the canvas
             [x0, y0, width, height] = map(float, self.viewBox.split())
+            # lower left corner in canvas coordinates
+            self.x0 = x0
+            self.y0 = y0
+            # width and height in canvas dimensions
+            self.width = width
+            self.height = height
             #dimension = self.dimension
             minside = min(width, height)
-            wscale = self.svg_width * 1.0/minside
-            swidth = wscale * width
-            hscale = self.svg_height * 1.0/minside
+            # scale from canvas width offset to device width offset
+            wscale = self.wscale = self.svg_width * 1.0/minside
+            # device width of draw area in device offsets
+            swidth = self.swidth = wscale * width
+            # scale from canvas height offset to device height offsert
+            hscale = self.hscale = self.svg_height * 1.0/minside
+            # height of draw area in device offsets
             sheight = hscale * height
             window = w.window()
             jQuery = window.jQuery
@@ -133,7 +186,13 @@ class HTML5CanvasProxy(traitlets.HasTraits):
         all_commands_json = json.dumps(all_commands)
         #p "encoded", len(all_commands_json)
         w(elt.context_execute(elt.canvas._get(0).getContext("2d"), all_commands_json))
+        self.operations = []
         w.flush()
+
+    def offset_to_point(self, offsetX, offsetY):
+        x = (offsetX * 1.0 / self.wscale) + self.x0
+        y = (offsetY * 1.0 / self.hscale) + self.y0
+        return (x, y)
 
     def rect(self, name, x, y, width, height, fill="black", event_cb=None, style_dict=None,
             **other_attributes):
